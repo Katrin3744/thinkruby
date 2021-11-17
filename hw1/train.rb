94ec561b9e14e0a11a9a3109b94f3db6b8b461c6
Katrin3744 <k.kukh3477@gmail.com>
2021-11-17 19:19:32 +0300

    New changes (rename) in train.rb

--- hw1/train.rb
+++ hw1/train.rb
@@ -1,6 +1,6 @@
 class Train
-  attr_reader :number_of_railway_carriage, :train_type, :train_route
-  attr_accessor :train_station
+  attr_reader :number_of_railway_carriage, :train_type, :route
+  attr_accessor :current_station
 
   def initialize(number, type, railway_carriage)
     @train_number = number
@@ -30,29 +30,29 @@ class Train
   end
 
   def train_route_add(route)
-    @train_route = route.stations
-    @train_station = @train_route.first
+    @route = route.stations
+    @current_station = @route.first
   end
 
   def transition_forward
-    @train_station = next_station
+    @current_station = next_station if next_station
   end
 
   def transition_back
-    @train_station = previous_station
+    @current_station = previous_station if previous_station
   end
 
   def previous_station
-    current_station = @train_route.find_index(@train_station)
-    if @train_station != @train_route.first
-      @train_route[current_station - 1]
+    current_dest = @route.find_index(@current_station)
+    if @current_station != @route.first
+      @route[current_dest - 1]
     end
   end
 
   def next_station
-    current_station = @train_route.find_index(@train_station)
-    if @train_station != @train_route.last
-      @train_route[current_station + 1]
+    current_dest = @route.find_index(@current_station)
+    if @current_station != @route.last
+      @route[current_dest + 1]
     end
   end
 
