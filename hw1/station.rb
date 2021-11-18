4332a333c9d97b5637ab451da72cb168cbd4368d
Katrin3744 <k.kukh3477@gmail.com>
2021-11-18 21:47:13 +0300

    hw2 done

--- hw1/station.rb
+++ hw1/station.rb
@@ -1,25 +1,21 @@
 class Station
-  attr_reader :trains
+  attr_reader :trains, :name
+
   def initialize(name)
-    @name=name
-    @trains=[]
+    @name = name
+    @trains = []
   end
+
   def add_train(train)
-    @trains<<train
-    train.train_route.include?(self)? train.train_station=self:puts("this train doesnt have this station in the route, please be careful") # если поезд прибывает на станцию, то его текущая станция внутри объекта тоже должна меняться
+    @trains << train
   end
-  def show_trains
-    @trains.each{|train|return train}
-  end
+
   def show_trains_with_type(type)
-    @trains.each do |train|
-      if train.train_type==type
-        return train
-      else puts "there is no such type of railway carriage in this train"
-      end
-    end
+    @trains.filter { |train| train.class.name == type }
   end
+
   def departure_train(train)
     @trains.delete(train)
   end
+
 end
